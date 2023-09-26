03188eb2700f23c6308c717de8092b424a208711
Adrian Grociak <ajgrociak@gmail.com>
2023-09-26 19:55:37 +0200

    Add if..else to script

--- js/script.js
+++ js/script.js
@@ -1,4 +1,43 @@
-let computerMove = `kamień`;
-let playerMove = 'papier';
+// Variables
 
-printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to papier, to wygrywasz!');
+let computerMove = `nieznany ruch`;
+let playerMove = 'nieznany ruch';
+
+// Draw number between 1 - 3
+let randomNumber = Math.floor(Math.random() * 3 + 1)
+
+// Ask a player to choose their move
+let playerInput = prompt('Wybierz swój ruch: 1: kamień, 2: papier, 3: nożyce');
+
+console.log('Wylosowana liczba to: ' + randomNumber);
+
+console.log('Gracz wylosował liczbę: ' + playerInput)
+
+if(randomNumber == 1) {
+    computerMove = 'kamień'
+} else if(randomNumber == 2) {
+    computerMove = 'papier'
+} else {
+    computerMove = 'nożyce'
+}
+
+if(playerInput == 1) {
+    playerMove = 'kamień'
+} else if(playerInput == 2) {
+    playerMove = 'papier'
+} else {
+    playerMove = 'nożyce'
+}
+
+printMessage('Mój ruch to: ' + computerMove)
+
+printMessage('Twój ruch to: ' + playerMove)
+
+// Display the result
+if(playerMove == computerMove) {
+    printMessage('Remis!')
+} else if (playerMove > computerMove) {
+    printMessage('Wygrałeś!')
+} else {
+    printMessage('Przegrałeś!')
+} 
